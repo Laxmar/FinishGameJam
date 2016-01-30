ddeecb5c456f2df6a98e10cbf227405fc4480a65
Piotr K <piotrkrawiranda@gmail.com>
2016-01-30 21:08:17 +0200

    bear prefab added

---
+++ Requiem/Assets/Skrypty/BearController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BearController : MonoBehaviour
+{
+
+    public float SpeedConstant;
+
+    private bool _facingRight = false;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+
+	void Start ()
+	{
+	    _rigidbody2D = GetComponent<Rigidbody2D>();
+	    _animator = GetComponent<Animator>();
+        if(!_facingRight) 
+            Flip();
+	}
+	
+	void FixedUpdate ()
+	{
+	    float move = Input.GetAxis("Horizontal");
+
+        _animator.SetFloat("Speed", Mathf.Abs(move));
+        
+       _rigidbody2D.velocity = new Vector3(move * SpeedConstant, _rigidbody2D.velocity.y);
+
+	    if (move > 0 && !_facingRight)
+            Flip();
+        else if( move < 0 && _facingRight)
+            Flip();
+
+	}
+
+    void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+}
