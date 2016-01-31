5a6507f9289956e1ccaf30a6ef5f382cf7ae92f0
Forys <forys13@gmail.com>
2016-01-31 02:15:01 +0200

    changeWorld script v2 + chapter 2

--- Requiem/Assets/Skrypty/WorldChanger.cs
+++ Requiem/Assets/Skrypty/WorldChanger.cs
@@ -5,23 +5,40 @@ public class WorldChanger : MonoBehaviour
 {
     public string BlackWorldMaskName;
     public string SunWorldMaskName;
-    public bool SwapWorldsFlag;
-    public bool SunWorldActive = true; //TODO make it private
+    public EdgeCollider2D SunWorldCollider;
+    public EdgeCollider2D BlackWorldCollider;
+
+    public Sprite SunWorldTexture;
+    public Sprite BlackWorldTexture;
+    public SpriteRenderer cameraBackground;
+
+    private bool SwapWorldsFlag = true;
+    private bool SunWorldActive = false; //TODO make it private
 
     private Camera _mainCamera;
 
     private void Start ()
 	{
 	    _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent(typeof (Camera)) as Camera;
+
+        HideMask(SunWorldMaskName);
+        SunWorldCollider.enabled = false;
+        cameraBackground.sprite = BlackWorldTexture;
+
         if (SunWorldActive)
         {
             HideMask(BlackWorldMaskName);
+            BlackWorldCollider.enabled = false;
+            cameraBackground.sprite = SunWorldTexture;
         }
 	}
 
     private void Update ()
 	{
-        SwapWorlds();//TODO delete these 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SwapWorlds();//TODO delete these 
+        }
 	}
 
     private void HideMask(string maskName)
@@ -40,18 +57,26 @@ public class WorldChanger : MonoBehaviour
         {
             if (SunWorldActive)
             {
+                SunWorldCollider.enabled = false;
+                BlackWorldCollider.enabled = true;
+                cameraBackground.sprite = BlackWorldTexture;
+
                 HideMask(SunWorldMaskName);
                 ShowMask(BlackWorldMaskName);
                 SunWorldActive = false;
             }
             else
             {
+                SunWorldCollider.enabled = true;
+                BlackWorldCollider.enabled = false;
+                cameraBackground.sprite = SunWorldTexture;
+
                 HideMask(BlackWorldMaskName);
                 ShowMask(SunWorldMaskName);
                 SunWorldActive = true;
             }
-            SwapWorldsFlag = false;
+            //SwapWorldsFlag = false;
         }
     }
-        
+
 }
